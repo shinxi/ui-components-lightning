34c02f5003cb26b5af0c08f46bfd50c903bb4bb3
Mallikarjuna Gurushanthappa <Mallikarjuna.Gurushanthappa@ge.com>
2018-01-26 17:04:11 -0800

    Checkbox and CheckboxGroup components refactoring, designed to work independently

--- stories/CheckboxStories.js
+++ stories/CheckboxStories.js
@@ -5,40 +5,71 @@ import { withInfo } from '@storybook/addon-info';
 import { text, boolean } from '@storybook/addon-knobs';
 import { CheckboxGroup, Checkbox } from './../lib';
 
+/**
+ * 
+ * @param {*} event object
+ */
+const onChange = (event) => {
+  console.log(event.target.value);
+};
+
+/**
+ * 
+ * @param {*} event object
+ * @param {*} values array
+ */
+const onChanges = (event, values) => {
+  console.log(event.target.value, values);
+};
+
 storiesOf('Checkbox', module)
-  .add('Controlled with knobs', withInfo('Checkbox controlled with knobs')(() => (
-    <CheckboxGroup
-      label={ text('label', 'Checkbox Group Label') }
-      error={ text('error') }
-      required={ boolean('required') }
-      onChange={ action('change') }
-    >
-      <Checkbox label='Checkbox Label One' value='1' disabled={ boolean('disabled #1', false) } checked={ boolean('checked #1', false) } />
-      <Checkbox label='Checkbox Label Two' value='2' disabled={ boolean('disabled #2', false) } checked={ boolean('checked #2', false) } />
-    </CheckboxGroup>
-  )))
-  .add('Default', withInfo('Checkbox Textarea control')(() => (
-    <CheckboxGroup label='Radio Group Label'>
-      <Checkbox label='Checkbox Label One' value='1' checked />
-      <Checkbox label='Checkbox Label Two' value='2' checked={ false } />
-    </CheckboxGroup>
-  )))
-  .add('Required', withInfo('Checkbox control with required attribute')(() => (
-    <CheckboxGroup label='Checkbox Group Label' required>
-      <Checkbox label='Checkbox Label One' value='1' checked />
-      <Checkbox label='Checkbox Label Two' value='2' />
-    </CheckboxGroup>
-  )))
-  .add('Error', withInfo('Checkbox control with error message')(() => (
-    <CheckboxGroup label='Checkbox Group Label' required error='This field is required'>
-      <Checkbox label='Checkbox Label One' value='1' checked />
-      <Checkbox label='Checkbox Label Two' value='2' />
-    </CheckboxGroup>
-  )))
-  .add('Disabled', withInfo('Checkbox control with disabled status')(() => (
-    <CheckboxGroup label='Checkbox Group Label'>
-      <Checkbox label='Checkbox Label One' value='1' disabled />
-      <Checkbox label='Checkbox Label Two' value='2' disabled />
-    </CheckboxGroup>
-  )))
-;
+  .add(
+    'Single Checkbox',
+    withInfo('Single Checkbox control')(() => (
+      <Checkbox
+        label="Click here to agree to terms and conditions."
+        value="1"
+        onChange={onChange}
+      />
+    )),
+  )
+  .add(
+    'Multi Checkbox',
+    withInfo('Checkbox control with different state')(() => (
+      <CheckboxGroup label="Radio Group Label" onChange={onChanges}>
+        <Checkbox label="Checkbox Label One" value="1" checked={false} />
+        <Checkbox label="Checkbox Label Two" value="2" checked={false} />
+      </CheckboxGroup>
+    )),
+  )
+  .add(
+    'Disabled Checkbox',
+    withInfo('Checkbox control with disabled status')(() => (
+      <CheckboxGroup label="Checkbox Group Label">
+        <Checkbox label="Checkbox Label One" value="1" disabled />
+        <Checkbox label="Checkbox Label Two" value="2" disabled />
+      </CheckboxGroup>
+    )),
+  )
+  .add(
+    'Controlled with knobs',
+    withInfo('Checkbox controlled with knobs')(() => (
+      <CheckboxGroup
+        label={text('label', 'Checkbox Group Label')}
+        onChange={action('change')}
+      >
+        <Checkbox
+          label="Checkbox Label One"
+          value="1"
+          disabled={boolean('disabled #1', false)}
+          checked={boolean('checked #1', false)}
+        />
+        <Checkbox
+          label="Checkbox Label Two"
+          value="2"
+          disabled={boolean('disabled #2', false)}
+          checked={boolean('checked #2', false)}
+        />
+      </CheckboxGroup>
+    )),
+  );
