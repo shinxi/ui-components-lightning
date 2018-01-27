099e09e8890bb464549be84606614b87a7d9cd87
josh sylvester <josh.sylvester@ge.com>
2018-01-26 16:02:48 -0800

    UI-430 worked through grid and row, need to finish item.

--- stories/GridStories.js
+++ stories/GridStories.js
@@ -19,16 +19,7 @@ Box.propTypes = {
 };
 
 storiesOf('Grid', module)
-  .add('Weighted', withInfo('columns with weighted width')(() => (
-    <Grid>
-      <Row cols={4}>
-        <Col cols={1}><Box>A: w=1</Box></Col>
-        <Col cols={2}><Box>B: w=2</Box></Col>
-        <Col cols={1}><Box>C: w=1</Box></Col>
-      </Row>
-    </Grid>
-  )))
-  .add('Equally Weighted', withInfo('columns with equally weighted')(() => (
+  .add('Equal Columns', withInfo('equal columns flex to the sides')(() => (
     <Grid>
       <Row>
         <Col><Box>A</Box></Col>
@@ -37,14 +28,23 @@ storiesOf('Grid', module)
       </Row>
     </Grid>
   )))
+  .add('Multiple Columns', withInfo('multiple columns, each column specified width, can specify any number of columns 1-6')(() => (
+    <Grid>
+      <Row cols={6}>
+        <Col cols={1}><Box>1/6</Box></Col>
+        <Col cols={2}><Box>2/6</Box></Col>
+        <Col cols={3}><Box>3/6</Box></Col>
+      </Row>
+    </Grid>
+  )))
   .add('Weighted, no-flex', withInfo('columns with weighted, flex is disabled')(() => (
     <Grid>
-      <Row>
-        <Col cols={1} noFlex><Box>A: w=1</Box></Col>
-        <Col cols={1} noFlex><Box>B: w=1</Box></Col>
-        <Col cols={2} noFlex><Box>C: w=2</Box></Col>
-        <Col cols={3} noFlex><Box>D: w=3</Box></Col>
-        <Col cols={3} noFlex><Box>E: w=3</Box></Col>
+      <Row cols={6}>
+        <Col cols={1} noFlex><Box>1/6 no flex</Box></Col>
+        <Col cols={1} noFlex><Box>1/6 no flex</Box></Col>
+        <Col cols={2} noFlex><Box>2/6 no flex</Box></Col>
+        <Col cols={3} noFlex><Box>3/6 no flex</Box></Col>
+        <Col cols={3} noFlex><Box>3/6 no flex</Box></Col>
       </Row>
     </Grid>
   )))
