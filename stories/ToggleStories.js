4701379cc316a13cc0f97b5acad08e782eb1dc2a
Hanzhang Luo <hanzhang.luo@ge.com>
2018-02-09 14:23:05 -0800

    Toggle component, unit test, storybook

--- stories/ToggleStories.js
+++ stories/ToggleStories.js
@@ -3,26 +3,64 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
 import { text, boolean } from '@storybook/addon-knobs';
-import { Toggle } from './../lib';
+import { Toggle } from '../lib';
+
+class ToggleWrapper extends React.Component {
+  state = { isChecked: false };
+
+  onChange = () => {
+    this.setState(prevState => ({ isChecked: !prevState.isChecked }));
+  };
+
+  render() {
+    return <Toggle isChecked={this.state.isChecked} onChange={this.onChange} />;
+  }
+}
 
 storiesOf('Toggle', module)
-  .add('Controlled with knobs', withInfo('Toggle controlled with knobs')(() => (
-    <Toggle
-      label={ text('label', 'Toggle Label') }
-      required={ boolean('required') }
-      error={ text('error') }
-      checked={ boolean('checked') }
-      disabled={ boolean('disabled') }
-      onChange={ action('change') }
-    />
-  )))
-  .add('Default', withInfo('Toggle control')(() => (
-    <Toggle onChange={ action('change') } />
-  )))
-  .add('Checked', withInfo('Toggle control with checked status')(() => (
-    <Toggle checked onChange={ action('change') } />
-  )))
-  .add('Disabled', withInfo('Toggle control with disabled status')(() => (
-    <Toggle disabled onChange={ action('change') } />
-  )))
-;
+  .add(
+    'Controlled with knobs',
+    withInfo('Toggle controlled with knobs')(() => (
+      <Toggle
+        label={text('label', 'Toggle Label')}
+        isRequired={boolean('isRequired')}
+        error={text('error')}
+        isChecked={boolean('isChecked')}
+        isDisabled={boolean('isDisabled')}
+        onChange={action('onChange')}
+      />
+    )),
+  )
+  .add(
+    'Default',
+    withInfo(`
+      Usage in a class component
+
+      ~~~js
+      class ToggleWrapper extends React.Component {
+        state = { isChecked: false };
+      
+        onChange = () => {
+          this.setState(prevState => ({ isChecked: !prevState.isChecked }));
+        };
+      
+        render() {
+          return <Toggle isChecked={this.state.isChecked} onChange={this.onChange} />;
+        }
+      }
+      ~~~
+
+    `)(() => <ToggleWrapper />),
+  )
+  .add(
+    'Checked',
+    withInfo('Toggle control with checked status')(() => (
+      <Toggle isChecked onChange={action('change')} />
+    )),
+  )
+  .add(
+    'Disabled',
+    withInfo('Toggle control with disabled status')(() => (
+      <Toggle isDisabled onChange={action('change')} />
+    )),
+  );
