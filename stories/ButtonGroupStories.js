b4e1d01d002962c7ff186edd80ad5a84e702706f
Sheela Narayanaswami <sheela.narayanaswami@ge.com>
2018-02-14 10:21:48 -0800

    UI-493-Button Group
    Button Group

--- stories/ButtonGroupStories.js
+++ stories/ButtonGroupStories.js
@@ -2,45 +2,47 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-import { ButtonGroup, Button, DropdownButton, MenuItem } from './../lib';
+import { ButtonGroup, Button, DropdownButton, Icon, MenuItem } from './../lib';
 
 const darkBgStyle = { backgroundColor: '#16325c', padding: 4 };
 
 const stories = storiesOf('ButtonGroup', module)
   .add('Default', withInfo('Default grouped buttons')(() => (
     <ButtonGroup>
-      <Button type='neutral' onClick={ action('click') }>Refresh</Button>
-      <Button type='neutral' onClick={ action('click') }>Edit</Button>
-      <Button type='neutral' icon='download' iconAlign='left' onClick={ action('click') }>
+      <Button type="neutral" onClick={action('click')}>Refresh</Button>
+      <Button type="neutral" onClick={action('click')}>Edit</Button>
+      <Button type="neutral" onClick={action('click')}>
+        <Icon icon="download" align="left" />
         Download
       </Button>
     </ButtonGroup>
   )))
   .add('Default disalbed', withInfo('Grouped buttons with disabled button')(() => (
     <ButtonGroup>
-      <Button type='neutral' onClick={ action('click') }>Refresh</Button>
-      <Button type='neutral' onClick={ action('click') } >Edit</Button>
+      <Button type="neutral" onClick={action('click')}>Refresh</Button>
+      <Button type="neutral" onClick={action('click')} >Edit</Button>
       <Button
-        type='neutral'
+        type="neutral"
         disabled
-        icon='download' iconAlign='left'
-        onClick={ action('click') }
+        onClick={action('click')}
       >
+        <Icon icon="download" align="left" />
         Download
       </Button>
     </ButtonGroup>
   )))
   .add('More', withInfo('Grouped buttons with dropdown button in right')(() => (
     <ButtonGroup>
-      <Button type='neutral' onClick={ action('click') }>Refresh</Button>
-      <Button type='neutral' onClick={ action('click') }>Edit</Button>
-      <Button type='neutral' icon='download' iconAlign='left' onClick={ action('click') }>
+      <Button type="neutral" onClick={action('click')}>Refresh</Button>
+      <Button type="neutral" onClick={action('click')}>Edit</Button>
+      <Button type="neutral" onClick={action('click')}>
+        <Icon icon="download" align="right" />
         Download
       </Button>
       <DropdownButton
-        type='icon-border'
-        menuAlign='right'
-        onMenuItemClick={ action('menuItemClick') }
+        type="icon-border"
+        menuAlign="right"
+        onMenuItemClick={action('menuItemClick')}
       >
         <MenuItem>Menu Item One</MenuItem>
         <MenuItem>Menu Item Two</MenuItem>
@@ -49,17 +51,18 @@ const stories = storiesOf('ButtonGroup', module)
     </ButtonGroup>
   )))
   .add('Inverse', withInfo('Grouped buttons with inversed color')(() => (
-    <div style={ darkBgStyle }>
+    <div style={darkBgStyle}>
       <ButtonGroup>
-        <Button type='inverse' onClick={ action('click') }>Refresh</Button>
-        <Button type='inverse' onClick={ action('click') }>Edit</Button>
-        <Button type='inverse' icon='download' iconAlign='left' onClick={ action('click') }>
+        <Button type="inverse" onClick={action('click')}>Refresh</Button>
+        <Button type="inverse" onClick={action('click')}>Edit</Button>
+        <Button type="inverse" onClick={action('click')}>
+          <Icon icon="download" align="left" />
           Download
         </Button>
         <DropdownButton
-          type='icon-border-inverse'
-          menuAlign='right'
-          onMenuItemClick={ action('menuItemClick') }
+          type="icon-border-inverse"
+          menuAlign="right"
+          onMenuItemClick={action('menuItemClick')}
         >
           <MenuItem>Menu Item One</MenuItem>
           <MenuItem>Menu Item Two</MenuItem>
