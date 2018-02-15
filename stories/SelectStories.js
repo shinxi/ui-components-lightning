35c58c96f2601218726dc52eb824ac54256e62f9
Sheela Narayanaswami <sheela.narayanaswami@ge.com>
2018-02-14 19:28:30 -0800

    update some minor changes

--- stories/SelectStories.js
+++ stories/SelectStories.js
@@ -5,6 +5,13 @@ import { action } from '@storybook/addon-actions';
 import { boolean } from '@storybook/addon-knobs';
 import { FormElement, Select, Option, Label } from '../lib';
 
+const optionsArrayObj = [
+  { value: 'option1', label: 'Option 1' },
+  { value: 'option2', label: 'Option 2' },
+  { value: 'option3', label: 'Option 3' },
+  { value: 'option4', label: 'Option 4' },
+];
+
 storiesOf('Select', module)
   .add('Controlled with knobs', withInfo('Select controlled with knobs')(() => (
     <FormElement>
@@ -71,6 +78,16 @@ storiesOf('Select', module)
         <Option label="Option Two" value="2" />
         <Option label="Option Three" value="3" />
       </Select>
+    </FormElement>
+  )))
+  .add('Default - with array Object', withInfo('Select with Array objects')(() => (
+    <FormElement>
+      <Label isRequired> Select label </Label>
+      <Select
+        onChange={action('change')}
+        onBlur={action('blur')}
+        optionsArray={optionsArrayObj}
+      />
     </FormElement>
   )))
 ;
